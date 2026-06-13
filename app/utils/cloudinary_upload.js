66a1f63f14e442dcbe78e509ec452bab95011349
kritikafashionpoint <kritikafashionpoint6@gmail.com>
2026-06-13 11:58:29 +0530

    new

--- app/utils/cloudinary_upload.js
+++ app/utils/cloudinary_upload.js
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
     storage,
-    limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+    limits: { fileSize: 100 * 1024 * 1024 }, // 100MB
 });
 
 export default upload;
